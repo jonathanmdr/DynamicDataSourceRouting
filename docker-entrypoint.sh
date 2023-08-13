fed202df818e42feb66a24c3b8a63efb219f6feb
Jonathan Henrique <jonathan.mdr@hotmail.com>
2023-08-13 12:50:54 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,13 @@
 set -e
 
 # Environment variables for development with docker
-export OTEL_TRACES_EXPORTER=jaeger
-export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://host.docker.internal:14250
-export OTEL_METRICS_EXPORTER=none
+export OTEL_METRICS_EXPORTER=otlp
+export OTEL_EXPORTER_OTLP_METRICS_COMPRESSION=gzip
+export OTEL_EXPORTER_OTLP_METRICS_ENDPOINT=http://host.docker.internal:4317
+export OTEL_TRACES_EXPORTER=otlp
+export OTEL_EXPORTER_OTLP_TRACES_COMPRESSION=gzip
+export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://host.docker.internal:4317
+export OTEL_LOGS_EXPORTER=none
 export OTEL_SERVICE_NAME=billionaire-api
 
 exec java -Dspring.profiles.active=docker -javaagent:opentelemetry-javaagent.jar -jar app.jar
